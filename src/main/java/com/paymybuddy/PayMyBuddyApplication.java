97ef03aee2525cc0c9f57c0e42231d43ab97396e
Noemie Menu <menunoemie.dev@outlook.com>
2021-04-22 11:40:57 +0200

    - create login.html and register.html pages

--- src/main/java/com/paymybuddy/PayMyBuddyApplication.java
+++ src/main/java/com/paymybuddy/PayMyBuddyApplication.java
@@ -2,8 +2,9 @@ package com.paymybuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class PayMyBuddyApplication {
 
     public static void main(String[] args) {
